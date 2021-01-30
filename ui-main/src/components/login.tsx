73f53095e6a89842add66e87cdf0fed690f6e157
Cullen Franklin <cullenfranklin@vt.edu>
2021-01-30 17:51:38 -0500

    started login forms

--- ui-main/src/components/login.tsx
+++ ui-main/src/components/login.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
-export interface LoginProps {
-    userType: String
+export interface LoginProps extends RouteComponentProps {
+    match: {
+        isExact: boolean,
+        path: string,
+        params: {
+            userType: string
+        },
+        url: string
+    }
 }
  
 export interface LoginState {
-    
+    userType: string,
+    loginInfo: {
+        displayName: string,
+        roomID: string
+    }
 }
  
 class Login extends React.Component<LoginProps, LoginState> {
-    state = { userType: '' }
+    state = { 
+        userType: '', 
+        loginInfo: {
+            displayName: '',
+            roomID: ''
+        } 
+    }
+
+    componentDidMount() {
+        this.setState({ userType: this.props.match.params.userType });
+    }
+
     render() { 
-        return ( <div>Login</div> );
+        return ( 
+            <React.Fragment>
+                <h1>Login {this.state.userType}</h1>
+                <form></form>
+            </React.Fragment> 
+        );
     }
 }
  
