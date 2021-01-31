6689fffe9e1e8e00f10c1e171f6ecd8490beac10
Cullen Franklin <cullenfranklin@vt.edu>
2021-01-30 21:33:03 -0500

    restructured room/login component

--- ui-main/src/App.tsx
+++ ui-main/src/App.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect
 } from 'react-router-dom';
 import Home from './components/home';
 import './App.css';
-import Login from './components/login';
+// import Login from './components/login';
+import Room from './components/room';
 
 function App() {
-  return (
-    <React.Fragment>
-      <Router>
-        <main className="container">
-          <Switch>
-            <Route path="/home" component={Home}/>
-            <Route path="/login/:userType" component={Login}/>
-            <Redirect from="/" to="/home"/>
-          </Switch>
-        </main>
-      </Router>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Router>
+				<main className="container">
+					<Switch>
+						<Route path="/home" component={Home} />
+						{/* <Route path="/login/:userType" component={Login}/> */}
+						<Route path="/room/:userType" component={Room} />
+						<Redirect from="/" to="/home" />
+					</Switch>
+				</main>
+			</Router>
+		</React.Fragment>
+	);
 }
 
 export default App;
--- ui-main/src/components/home.tsx
+++ ui-main/src/components/home.tsx
@@ -2,34 +2,36 @@ import React from 'react';
 import About from './about';
 import { Link } from 'react-router-dom';
 
-export interface HomeProps {
-    
-}
- 
-export interface HomeState {
-    
-}
- 
+export interface HomeProps {}
+
+export interface HomeState {}
+
 class Home extends React.Component<HomeProps, HomeState> {
-    render() { 
-        return ( 
-            <React.Fragment>
-                <div className="px-3 role-prompt">
-                    <h1>What is your role?</h1>
-                    <p className="lead">Please select a role to login to a lecture with.</p>
-                    <div className="container">
-                        <Link to="/login/student">
-                            <button className="btn btn-primary mr-3">Student</button>
-                        </Link>
-                        <Link to="/login/teacher">
-                            <button className="btn btn-primary ml-3">Teacher</button>
-                        </Link>
-                    </div>
-                </div>
-                <About/>
-            </React.Fragment> 
-        );
-    }
+	render() {
+		return (
+			<React.Fragment>
+				<div className="px-3 role-prompt">
+					<h1>What is your role?</h1>
+					<p className="lead">
+						Please select a role to login to a lecture with.
+					</p>
+					<div className="container">
+						<Link to="/room/student">
+							<button className="btn btn-primary mr-3">
+								Student
+							</button>
+						</Link>
+						<Link to="/room/teacher">
+							<button className="btn btn-primary ml-3">
+								Teacher
+							</button>
+						</Link>
+					</div>
+				</div>
+				<About />
+			</React.Fragment>
+		);
+	}
 }
- 
+
 export default Home;
