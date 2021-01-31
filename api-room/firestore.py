332a5b9c166bb45782144061f25582bf7ff92efc
Matt Davison <mattd7@vt.edu>
2021-01-30 23:17:40 -0500

    upload and gauge faces

--- api-room/firestore.py
+++ api-room/firestore.py
@@ -1,7 +1,9 @@
 from google.cloud import firestore
 import os
+import time
 
 db = firestore.Client()
+
 
 def get_room_by_id(id):
     doc = db.collection(u'rooms').document(id).get()
@@ -10,10 +12,24 @@ def get_room_by_id(id):
     else:
         return None
 
-def add_room(room_id, host_ip, lecture_name, host_name):
+def add_room(room_id, host_ip, lecture_name, host_username):
     data = {
         u'host_ip' : host_ip,
         u'room_name' : lecture_name,
-        u'host_name' : host_name
+        u'host_username' : host_username,
+        u'created_at': time.time()
     }
     db.collection(u'rooms').document(room_id).set(data)
+
+def add_to_room(room_id, stu_name):
+    doc = db.collection(u'rooms').document(room_id).update({u'students': firestore.ArrayUnion([stu_name])})
+
+def make_gauge_doc(room_id, gauge_time):
+    db.collection(u'rooms').document(room_id).collection(u'gauges').document(gauge_time).set({u'students': []})
+
+def save_gauge(stu_name, reactions, room_id, guage_time):
+    student = {"stu_name": stu_name,
+        "reactions": reactions
+    }
+    doc = db.collection(u'rooms').document(room_id).collection(u'gauges').document(guage_time).update({u'students': firestore.ArrayUnion([student])})
+
